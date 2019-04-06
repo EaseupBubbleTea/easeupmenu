cf3da196c49e39ca64a57a5d59388c4b4a08d4d3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-06 04:40:23 +0000

    Deploy EaseupBubbleTea/easeupmenu to github.com/EaseupBubbleTea/easeupmenu.git:gh-pages

---
+++ precache-manifest.0edd98f7635f739b7d3fb1bf8e85fbe7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45654335a63d3d0c5182",
+    "url": "/easeupmenu/static/css/main.66f9ddcc.chunk.css"
+  },
+  {
+    "revision": "45654335a63d3d0c5182",
+    "url": "/easeupmenu/static/js/main.91d386bd.chunk.js"
+  },
+  {
+    "revision": "b6a86cc86502cda6bd02",
+    "url": "/easeupmenu/static/js/runtime~main.08ecad9b.js"
+  },
+  {
+    "revision": "d91c98fe5b6cbd39f191",
+    "url": "/easeupmenu/static/js/2.dbcac54c.chunk.js"
+  },
+  {
+    "revision": "eab4531987dc02c875bc5bb0cf966cfd",
+    "url": "/easeupmenu/index.html"
+  }
+];
