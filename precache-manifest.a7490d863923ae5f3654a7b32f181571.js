fc2fcc1fdda767437cc3897a9f9a3fc4b5868e7d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-07 00:27:00 +0000

    Deploy EaseupBubbleTea/easeupmenu to github.com/EaseupBubbleTea/easeupmenu.git:gh-pages

---
+++ precache-manifest.a7490d863923ae5f3654a7b32f181571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1af9e3b8b8bf5780fa09",
+    "url": "/easeupmenu/static/css/main.30bca581.chunk.css"
+  },
+  {
+    "revision": "1af9e3b8b8bf5780fa09",
+    "url": "/easeupmenu/static/js/main.fe139853.chunk.js"
+  },
+  {
+    "revision": "b6a86cc86502cda6bd02",
+    "url": "/easeupmenu/static/js/runtime~main.08ecad9b.js"
+  },
+  {
+    "revision": "d91c98fe5b6cbd39f191",
+    "url": "/easeupmenu/static/js/2.dbcac54c.chunk.js"
+  },
+  {
+    "revision": "e4f3771b743e55227e4d19062de9e112",
+    "url": "/easeupmenu/static/media/greenTea.e4f3771b.jpg"
+  },
+  {
+    "revision": "ad871156d6420f92003fd3600e1c7b6c",
+    "url": "/easeupmenu/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/easeupmenu/precache-manifest.1909a867139e17b853b8174f1f013c46.js"
+  "/easeupmenu/precache-manifest.a7490d863923ae5f3654a7b32f181571.js"
 );
 
 workbox.clientsClaim();
