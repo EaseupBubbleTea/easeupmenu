98358bdf9f918d076952882a6e7fdd99b3b36f4b
easeupBubbletea <stylishcreationdress@gmail.com>
2019-04-07 08:29:36 +0700

    wip

--- src/App.js
+++ src/App.js
@@ -28,13 +28,25 @@ class App extends Component {
               </div>
               <div className="section">
                 <div className="slide">
-                    <div className="menuItem menuItem__greentea">                      
-                    </div>
+                  <div className="menuItem menuItem__greentea">
+                  </div>
                 </div>
                 <div className="slide">
-                  <h3>ราคา</h3>
-                  <h2>เล็ก 40</h2>
-                  <h2>กลาง 50</h2>
+                  <div className="item-detail">
+                    <p className="item-detail item-detail__signature">
+                      เพราะรสชาดและคุณภาพคือเรื่องสำคัญสูงสุดสำหรับเรา
+                      จึงเป็นจุดเริ่มต้นของการเดินทางสรรหาวัตถุดิบถึงต้นตำหรับที่ไถหนาน ไต้หวัน
+                      นำมาผสานกับประสบการณ์เฉพาะในแบบฉบับของ EaseUp
+                      จนได้ชานมคุณภาพอย่างที่เราภูมิใจ
+                    </p>
+                  </div>
+                </div>
+                <div className="slide">
+                  <div className="item-price">
+                    <h1>ราคา</h1>
+                    <p>เล็ก 40 บาท</p>
+                    <p>ใหญ่ 50 บาท</p>
+                  </div>
                 </div>
               </div>
               <div className="section">
